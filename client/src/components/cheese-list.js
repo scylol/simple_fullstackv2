d66e6af1e7ec93870c8aa557b10fd3d9d9756e0d
Scylol <djd3dobbin@gmail.com>
2017-05-26 17:16:57 -0400

    working version

---
+++ client/src/components/cheese-list.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {fetchCheeses} from '../actions/cheese'
+
+export class CheeseList extends React.Component {
+    
+    componentDidMount() {
+        this.props.dispatch(fetchCheeses());
+    };
+ render() {
+    const cheeses = this.props.cheeses.map((cheese, index) => (
+        <li key={index}>
+            {cheese}
+        </li>    
+    ));
+
+    return (
+        <ul id="cheeseList">
+            {cheeses}
+        </ul>    
+    );
+ }
+};
+
+const mapStateToProps = state => ({
+    cheeses: state.cheeses
+});
+
+export default connect(mapStateToProps)(CheeseList);
